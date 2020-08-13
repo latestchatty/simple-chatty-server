dc431a68e8be5bf38be926a720b46bc93c9ded88
Brian Luft <brian@electroly.com>
2020-08-13 06:30:04 +0000

    Startup: disable default stuff we don't need

--- src/Startup.cs
+++ src/Startup.cs
@@ -39,21 +39,8 @@ namespace SimpleChattyServer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
             app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
     }
 }
