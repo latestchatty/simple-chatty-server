46b4c1a4f8f9d0888baed865668dd4a194079f75
Brian Luft <brian@electroly.com>
2020-08-16 17:18:54 +0000

    t2.nano -> t2.micro

--- terraform/server.tf
+++ terraform/server.tf
@@ -15,7 +15,7 @@ data "aws_ami" "latest_ubuntu" {
 
 resource "aws_instance" "simple_chatty_server" {
   ami = data.aws_ami.latest_ubuntu.id
-  instance_type = "t2.nano"
+  instance_type = "t2.micro"
   disable_api_termination = false
   instance_initiated_shutdown_behavior = "stop"
   monitoring = false
