72ee2f20379da2cf57ccfaeb3a52b30c87b583fa
Brian Luft <brian@electroly.com>
2020-08-16 07:58:50 +0000

    Fix exception filter

--- src/HttpResponseExceptionFilter.cs
+++ src/HttpResponseExceptionFilter.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,33 +18,36 @@ namespace SimpleChattyServer
 
         public void OnActionExecuted(ActionExecutedContext context)
         {
-            var logger = context.HttpContext.RequestServices.GetService<ILogger<HttpResponseExceptionFilter>>();
-            logger.LogError(context.Exception, context.Exception.Message);
-
-            if (context.Exception is ApiException apiException)
+            if (context.Exception != null)
             {
-                var response =
-                    new ErrorResponse
-                    {
-                        Code = apiException.Code,
-                        Message = apiException.Message
-                    };
+                var logger = context.HttpContext.RequestServices.GetService<ILogger<HttpResponseExceptionFilter>>();
+                logger.LogError(context.Exception, context.Exception.Message);
 
-                context.Result = new ObjectResult(response) { StatusCode = apiException is Api400Exception ? 400 : 500 };
+                if (context.Exception is ApiException apiException)
+                {
+                    var response =
+                        new ErrorResponse
+                        {
+                            Code = apiException.Code,
+                            Message = apiException.Message
+                        };
+
+                    context.Result = new ObjectResult(response) { StatusCode = apiException is Api400Exception ? 400 : 500 };
+                }
+                else
+                {
+                    var response =
+                        new ErrorResponse
+                        {
+                            Code = Api500Exception.Codes.SERVER,
+                            Message = context.Exception.Message
+                        };
+
+                    context.Result = new ObjectResult(response) { StatusCode = 500 };
+                }
+
+                context.ExceptionHandled = true;
             }
-            else
-            {
-                var response =
-                    new ErrorResponse
-                    {
-                        Code = Api500Exception.Codes.SERVER,
-                        Message = context.Exception.Message
-                    };
-
-                context.Result = new ObjectResult(response) { StatusCode = 500 };
-            }
-
-            context.ExceptionHandled = true;
         }
     }
 }
