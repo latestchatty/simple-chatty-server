fcfa14fab26a7f2584427fd994bc2fcca777dc65
Brian Luft <brian@electroly.com>
2020-08-19 00:54:16 +0000

    Don't blow up a response because we don't understand a timezone

--- src/Data/PacificTimeZone.cs
+++ src/Data/PacificTimeZone.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Runtime.InteropServices;
-using SimpleChattyServer.Exceptions;
 
 namespace SimpleChattyServer.Data
 {
@@ -14,11 +13,11 @@ namespace SimpleChattyServer.Data
         public static string GetAbbreviationFromOffset(TimeSpan offset) =>
             offset.Hours == -7 ? "PDT" :
             offset.Hours == -8 ? "PST" :
-            throw new Api500Exception(Api500Exception.Codes.SERVER, $"Time zone offset {offset} is neither PDT nor PST.");
+            "UTC";
 
         public static TimeSpan GetOffsetFromAbbreviation(string abbreviation) =>
             abbreviation == "PDT" ? TimeSpan.FromHours(-7) :
             abbreviation == "PST" ? TimeSpan.FromHours(-8) :
-            throw new Api500Exception(Api500Exception.Codes.SERVER, $"Time zone {abbreviation} must be either PDT or PST.");
+            TimeSpan.FromHours(0);
     }
 }
