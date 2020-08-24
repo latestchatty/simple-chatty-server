947bc983d24b338e000561b32b13149bc203fb79
Brian Luft <brian@electroly.com>
2020-08-24 06:37:16 +0000

    Use systemd lifetime

--- src/Program.cs
+++ src/Program.cs
@@ -7,11 +7,15 @@ namespace SimpleChattyServer
 {
     public static class Program
     {
-        public static void Main(string[] args) =>
-            CreateHostBuilder(args).Build().Run();
+        public static void Main(string[] args)
+        {
+            using var host = CreateHostBuilder(args).Build();
+            host.Run();
+        }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSystemd()
                 .ConfigureLogging((context, builder) =>
                 {
                     builder.ClearProviders();
