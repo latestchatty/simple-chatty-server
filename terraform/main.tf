fb700d4cd5e0c7ced4419ffecccd2b225bc40ffc
Brian Luft <brian@electroly.com>
2020-11-19 03:31:45 +0000

    Update to terraform 0.13

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,14 @@
 terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.16.0"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = "3.0.0"
+    }
+  }
   backend "s3" {
     bucket = "simple-chatty-server-terraform"
     key = "simple-chatty-server"
@@ -7,10 +17,9 @@ terraform {
 }
 
 provider "aws" {
-  version = "~> 2.19"
   region = "us-east-1"
 }
 
 provider "null" {
-  version = "~> 2.1"
 }
+
