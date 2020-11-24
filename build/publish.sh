01f874be91a093fae0739cfefc0b11c0a4fc7110
Brian Luft <brian@electroly.com>
2020-11-24 20:51:49 +0000

    Don't specify runtime-identifier in the csproj

--- build/publish.sh
+++ build/publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euxo pipefail
 mkdir -p publish
-dotnet publish ../src/SimpleChattyServer.csproj -c Release --self-contained -o publish/
+dotnet publish ../src/SimpleChattyServer.csproj -c Release --self-contained -o publish/ -r linux-arm64
 rm -f publish/*.pdb publish/*.config publish/appsettings.Development.json
