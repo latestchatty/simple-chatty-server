53941f98286e83f698bc85dc4a616b3c5239c268
Brian Luft <brian@electroly.com>
2020-11-24 20:55:09 +0000

    Don't specify PublishSingleFile in the csproj

--- build/publish.sh
+++ build/publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euxo pipefail
 mkdir -p publish
-dotnet publish ../src/SimpleChattyServer.csproj -c Release --self-contained -o publish/ -r linux-arm64
+dotnet publish ../src/SimpleChattyServer.csproj -c Release --self-contained -o publish/ -r linux-arm64 -p:PublishSingleFile=true
 rm -f publish/*.pdb publish/*.config publish/appsettings.Development.json
